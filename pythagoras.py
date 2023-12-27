dff3f5cc6492485ef608d15fba8be2cb4ff1caf5
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-12-27 23:08:39 +0100

    created first square

--- pythagoras.py
+++ pythagoras.py
@@ -1,26 +1,87 @@
 # Credit to https://www.youtube.com/watch?v=yfGtbNgcrQ8 for the proof
 
+import math
 from manim import *
 
 class Pythagoras(Scene):
     def construct(self):
-        a = Line(start=[0,0,0], end=[3,0,0]).set_color(BLUE)
-        b = Line(start=[3,0,0], end=[3,4,0]).set_color(GREEN)
-        c = Line(start=[0,0,0], end=[3,4,0]).set_color(RED)
+        a_line = Line(start=[0,0,0], end=[3,0,0]).set_color(BLUE)
+        a_label = Tex("a").move_to(a_line.get_center() + 0.5*DOWN)
+        a = VGroup(a_line, a_label)
 
-        right_triangle = VGroup(a, b, c).scale(0.5)
+        b_line = Line(start=[3,0,0], end=[3,4,0]).set_color(GREEN)
+        b_label = Tex("b").next_to(b_line.get_center() + 0.05*RIGHT)
+        b = VGroup(b_line, b_label)
 
-        def labels():
-            a_label = Tex("a").next_to(right_triangle, DOWN)
-            b_label = Tex("b").next_to(right_triangle, RIGHT)
-            c_label = Tex("c").move_to(right_triangle.get_center() + LEFT*0.5)
+        c_line = Line(start=[0,0,0], end=[3,4,0]).set_color(RED)
+        c_label = Tex("c").move_to(c_line.get_center() + LEFT*0.5)
+        c = VGroup(c_line, c_label)
 
-            return VGroup(a_label, b_label, c_label)
-        
-        label = always_redraw(labels)
+        right_triangle_labeled = VGroup(a, b, c).scale(0.5)
 
-        self.play(Create(VGroup(right_triangle, label)))
-        self.wait(duration=3)
-        self.play(right_triangle.animate.to_edge(UL))
+        def square_1(a_line, b_line):
+            a_sqr = a_line.copy()
+            b_sqr = b_line.copy()
+            self.play(Rotate(b_sqr, 1/2 * math.pi))
+            self.play(a_sqr.animate.move_to([-0.75,0,0]), b_sqr.animate.move_to([1,0,0]))
 
-        
+            ab1 = VGroup(a_sqr, b_sqr)
+
+            ab2 = ab1.copy()
+            self.play(Rotate(ab2, - 1/2 * math.pi))
+            self.play(ab2.animate.move_to([2, -1.75, 0]))
+
+            ab3 = ab2.copy()
+            self.play(Rotate(ab3, 1/2 * math.pi))
+            self.play(ab3.animate.move_to([0.25, -3.5, 0]))
+
+            ab4 = ab3.copy()
+            self.play(Rotate(ab4, - 1/2 * math.pi))
+            self.play(ab4.animate.move_to([-1.5, -1.75, 0]))
+
+            return VGroup(ab1, ab2, ab3, ab4)
+
+
+        def insert_triangles1(right_triangle):
+
+            right_triangle1 = right_triangle.copy()
+            self.play(Rotate(right_triangle1, 1/2 * math.pi))
+            self.play(right_triangle1.animate.move_to([1, -0.75,0]))
+
+            right_triangle2 = right_triangle.copy()
+            self.play(Rotate(right_triangle2, -1/2 * math.pi))
+            self.play(right_triangle2.animate.move_to([1,-0.75,0]))
+
+            right_triangle3 = right_triangle.copy()
+            self.play(right_triangle3.animate.move_to([-0.75,-2.5,0]))
+
+            right_triangle4 = right_triangle.copy()
+            self.play(Rotate(right_triangle4, math.pi))
+            self.play(right_triangle4.animate.move_to([-0.75,-2.5,0]))
+
+            a_sqr_text = MathTex("a^2").move_to(right_triangle1.get_center() + 1.75*LEFT).set_color(BLUE)
+            b_sqr_text = MathTex("b^2").move_to(right_triangle3.get_center() + 1.75*RIGHT).set_color(GREEN)
+            self.play(Create(VGroup(a_sqr_text, b_sqr_text)))
+
+            return VGroup(right_triangle1, right_triangle2, right_triangle3, right_triangle4, a_sqr_text, b_sqr_text)
+
+
+
+        # play animations
+        self.play(Create(right_triangle_labeled))
+        self.wait()
+        self.play(right_triangle_labeled.animate.to_edge(UL))
+
+        right_triangle = VGroup(a_line, b_line, c_line)
+
+        square1 = square_1(a_line, b_line)
+        self.wait()
+
+        triangles1 = insert_triangles1(right_triangle)
+        self.wait()
+
+        filled_square = VGroup(triangles1, square1)
+        self.play(filled_square.animate.to_edge(DL))
+
+        self.wait()
+
