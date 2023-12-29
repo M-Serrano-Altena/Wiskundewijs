d7f08f3a3b2b60a8834581bc9f9e7cfd316b4e02
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-12-29 14:03:41 +0100

    exponential derivative

--- Derivative/derivative.py
+++ Derivative/derivative.py
@@ -1,7 +1,7 @@
 from manim import *
+import numpy as np
 
 class Parabola(Scene):
-    
     def construct(self):
         num = ValueTracker(-4)
         dx = 0.00001
@@ -46,4 +46,51 @@ class Parabola(Scene):
         self.play(num.animate.set_value(0), run_time=5)
         self.wait()
         self.play(num.animate.set_value(4), run_time = 5)
+        self.wait()
+
+
+class Exponential(Scene):
+    def construct(self):
+        num = ValueTracker(-4)
+        dx = 0.00001
+
+        function_label = always_redraw(lambda : MathTex(f"f(x = {num.get_value():.2f}) = e^{{{(num.get_value()):.2f}}} = {np.exp(num.get_value()):.2f}").to_edge(UL))
+        derivative_label = always_redraw(lambda : MathTex(f"f'(x = {num.get_value():.2f}) = {np.exp(num.get_value()):.2f}").next_to(function_label, DOWN))
+        
+        plane = NumberPlane(
+            x_range=[-4, 4, 2], x_length=8, y_range=[0, 60, 10], y_length=6
+        ).add_coordinates()
+
+        labels = plane.get_axis_labels(x_label="x", y_label="f(x)")
+
+        exp = plane.plot(lambda x: np.exp(x), x_range=[-4, 4], color=GREEN)
+        func_label = (
+            MathTex("f(x) = e^x \\\\ f'(x) = e^x")
+            .scale(0.6)
+            .next_to(exp, UR, buff=0.5)
+            .set_color(GREEN)
+        )
+
+        slope = always_redraw(
+            lambda: plane.get_secant_slope_group(
+                x=num.get_value(),
+                graph=exp,
+                dx=dx,
+                secant_line_color=RED,
+                secant_line_length=3,
+            )
+        )
+
+        point = always_redraw(
+            lambda: Dot(color=GREEN).move_to(
+                plane.c2p(num.get_value(), exp.underlying_function(num.get_value()))
+            )
+        )
+
+        slope_label = always_redraw(lambda : Tex(f"slope = {((exp.underlying_function(num.get_value() + dx) - exp.underlying_function(num.get_value())) / dx):.2f}").next_to(derivative_label, DOWN))
+
+        self.play(DrawBorderThenFill(plane))
+        self.play(Create(VGroup(exp, labels, func_label, slope, point, function_label, derivative_label, slope_label)), run_time=7)
+        self.wait()
+        self.play(num.animate.set_value(4), run_time=10)
         self.wait()
